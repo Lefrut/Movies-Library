07c3e7664724ce552b6ba84a6f38814f563e3f0d
Kirill Dashkevich <programmerdashkevich@gmail.com>
2022-12-06 12:10:20 +0300

    fix di

--- app/src/main/java/ru/dashkevich/viewapp/di/RepositoryModule.kt
+++ app/src/main/java/ru/dashkevich/viewapp/di/RepositoryModule.kt
@@ -1,14 +1,12 @@
 package ru.dashkevich.viewapp.di
 
-import android.content.Context
 import org.koin.dsl.module
 import ru.dashkevich.viewapp.data.repository.DataStoreRepository
 import ru.dashkevich.viewapp.data.repository.MoviesRepository
-import ru.dashkevich.viewapp.util.constants.dataStore
 
 val repositoryModule = module {
 
     single { MoviesRepository(get()) }
-    single { DataStoreRepository(get<Context>().dataStore) }
+    single { DataStoreRepository(get()) }
 
 }
