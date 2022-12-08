ec9dd59449540f8099db06f01f6e548f7d7d1bdb
Kirill Dashkevich <programmerdashkevich@gmail.com>
2022-12-08 15:14:44 +0300

    base profile screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
         classpath(Dependencies.ClassPath.gradlePlugin)
         classpath(Dependencies.ClassPath.kotlinGradlePlugin)
         classpath(Dependencies.ClassPath.navigationSafeArgsPlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     }
 }
 
--- features/profile/src/main/java/ru/dashkevich/profile/ProfileFragment.kt
+++ features/profile/src/main/java/ru/dashkevich/profile/ProfileFragment.kt
@@ -3,9 +3,13 @@ package ru.dashkevich.profile
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import ru.dashkevich.profile.adapter.ProfileTabsAdapter
 import ru.dashkevich.profile.databinding.FragmentProfileBinding
+import ru.dashkevich.profile.tabs.SavedFragment
+import ru.dashkevich.profile.tabs.SettingsFragment
 
-class ProfileFragment: Fragment(R.layout.fragment_profile){
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private lateinit var binding: FragmentProfileBinding
 
@@ -13,6 +17,14 @@ class ProfileFragment: Fragment(R.layout.fragment_profile){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProfileBinding.bind(view)
+
+        val tabs = listOf(Pair(SavedFragment(), "Сохранено"), Pair(SettingsFragment(), "Настройки"))
+
+        val profileTabsAdapter = ProfileTabsAdapter(this, tabs)
+        binding.pager.adapter = profileTabsAdapter
+        TabLayoutMediator(binding.tabLayout, binding.pager){ itemTab, position ->
+            itemTab.text = tabs[position].second
+        }.attach()
     }
 
 }
